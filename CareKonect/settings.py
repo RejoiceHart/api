ecf29d6de14f50fa8462a4d09a6dfb0020ea8315
Rejoice Hart <rejoicehart6@gmail.com>
2024-08-09 15:36:28 +0100

    Your commit message here

--- CareKonect/settings.py
+++ CareKonect/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",  #newly added
 ]
 
 ROOT_URLCONF = 'CareKonect.urls'
@@ -147,6 +148,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- User/models.py
+++ User/models.py
@@ -2,6 +2,3 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-# class Teacher(models.Model):
-#     name = models.CharField(max_length=80)
-#     age = models.IntegerField()
